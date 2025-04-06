a46ef093ed7571f9d21dc886f6b98b799b504242
yangnan <1962155093@qq.com>
2025-04-06 12:37:55 +0800

    Redis实现全局唯一订单ID生成

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,46 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+@Component
+public class RedisIdWorker {
+    /**
+     * 开始时间戳
+     * 2525-1-1
+     */
+    private static final long BEGIN_TIMESTAMP = 1735689600L;
+
+    /**
+     *序列号位数
+     */
+    private static final int COUNT_BITS = 32;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    public long  nextId(String keyPrefix){
+        //1.生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(java.time.ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+        //2.生成序列号
+        //2.1获取当天日期
+        String date = now.format(java.time.format.DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        //2。2自增长
+        long count = stringRedisTemplate.opsForValue().increment("icr:"+keyPrefix+":"+date);
+        //3.拼接并返回
+        return timestamp<< COUNT_BITS | count; //符号位+31位时间戳+32位序列号
+    }
+
+//    public static void main(String[] args) {
+//        //获取开始时间戳
+//        LocalDateTime time = LocalDateTime.of(2025, 1, 1, 0, 0, 0);
+//        long l = time.toEpochSecond(java.time.ZoneOffset.UTC);
+//        System.out.println(l);
+//    }
+}
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -5,13 +5,22 @@ import com.hmdp.service.impl.ShopServiceImpl;
 
 import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RedisIdWorker;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.Stack;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+@Data
 @SpringBootTest(classes = com.hmdp.HmDianPingApplication.class)
 public class HmDianPingApplicationTests {
     @Autowired
@@ -19,6 +28,31 @@ public class HmDianPingApplicationTests {
     @Resource
     private CacheClient cacheClient;
 
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+
+    //用多线程测试订单号生成
+    @Test
+    public void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
     @Test
     public void test() {
         shopService.saveShop2Redis(1L, 10L);
